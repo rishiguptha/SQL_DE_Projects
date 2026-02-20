fb4bbc20e4f327b6600cc2f6bea03bfe5b68487d
rishiguptha <guptamankala@gmail.com>
2026-02-20 15:07:03 -0600

    Data Validation queries

--- DW_MART_BUILD/02_load_schema_dw.sql
+++ DW_MART_BUILD/02_load_schema_dw.sql
@@ -31,3 +31,26 @@ INSERT INTO skills_job_dim(skill_id, job_id)
 SELECT skill_id, job_id
 FROM read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv', 
     AUTO_DETECT = TRUE);
+
+--Verifying the table record counts
+SELECT 'Company Dim' AS table_name , COUNT(*) AS record_count FROM company_dim
+UNION ALL
+SELECT 'Skills Dim' , COUNT(*) FROM skills_dim
+UNION ALL
+SELECT 'Job Postings Fact' , COUNT(*) FROM job_postings_fact
+UNION ALL
+SELECT 'Skills Job Dim' , COUNT(*) FROM skills_job_dim;
+
+
+-- Show sample data
+SELECT '=== Company Dimension Sample ===' AS info;
+SELECT * FROM company_dim LIMIT 5;
+
+SELECT '=== Skills Dimension Sample ===' AS info;
+SELECT * FROM skills_dim LIMIT 5;
+
+SELECT '=== Job Postings Fact Sample ===' AS info;
+SELECT * FROM job_postings_fact LIMIT 5;
+
+SELECT '=== Skills Job Bridge Sample ===' AS info;
+SELECT * FROM skills_job_dim LIMIT 5;
--- DW_MART_BUILD/build_dw_marts.sql
+++ DW_MART_BUILD/build_dw_marts.sql
@@ -1,3 +1,4 @@
+-- duckdb data/dw_marts.duckdb -c ".read build_dw_marts.sql"
 
 -- Step 1: DW - Create start schema tables
 .read 01_create_tables_dw.sql
